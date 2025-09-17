957d635bc1afc1ea68a3b1e5d44ac6683ac0f358
sashastahiev <vebium@mail.ru>
2025-09-18 00:22:21 +0300

    create skypro-music and make hw1

---
+++ src/App.vue
@@ -0,0 +1,32 @@
+<script setup>
+import FilterControls from './components/FilterControls.vue'
+import Navbar from './components/Navbar.vue'
+import PlayerBar from './components/PlayerBar.vue'
+import Playlist from './components/Playlist.vue'
+import Track from './components/Track.vue'
+</script>
+
+<template>
+  <div class="wrapper">
+    <div class="container">
+      <main class="main">
+        <Navbar />
+        <div class="main__centerblock centerblock">
+          <div class="centerblock__search search">
+            <svg class="search__svg">
+              <use xlink:href="./img/icon/sprite.svg#icon-search"></use>
+            </svg>
+            <input class="search__text" type="search" placeholder="Поиск" name="search" />
+          </div>
+          <FilterControls />
+          <Playlist />
+        </div>
+        <Track />
+      </main>
+      <PlayerBar />
+      <footer class="footer"></footer>
+    </div>
+  </div>
+</template>
+
+<style scoped></style>
