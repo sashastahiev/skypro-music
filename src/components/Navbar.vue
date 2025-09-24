1bd43fc0d424e62d75c5c83ca47efff8716c427c
sashastahiev <vebium@mail.ru>
2025-09-24 15:01:42 +0300

    commit for 24.09

--- src/components/Navbar.vue
+++ src/components/Navbar.vue
@@ -24,3 +24,70 @@
   </nav>
 </template>
 
+<style scoped>
+/* Применяется к навигационному меню */
+.main__nav {
+  width: 244px;
+  background-color: #181818;
+  padding: 20px 0 20px 36px;
+}
+
+/* Стиль контейнера логотипа */
+.nav__logo {
+  width: 113.33px;
+  height: 43px;
+  padding: 13px 0 13px 0;
+  background-color: transparent;
+  margin-bottom: 20px;
+}
+
+/* Логотип изображения */
+.logo__image {
+  width: 113.33px;
+  height: 17px;
+  color: #181818;
+}
+
+/* Контейнер гамбургера */
+.nav__burger {
+  width: 20px;
+  height: 36px;
+  padding: 13px 0;
+  display: flex;
+  flex-direction: column;
+  justify-content: space-between;
+}
+
+/* Линия гамбургера */
+.burger__line {
+  display: inline-block;
+  width: 100%;
+  height: 1px;
+  background-color: #d3d3d3;
+}
+
+/* Меню навигации */
+.nav__menu {
+  display: block;
+  visibility: visible;
+}
+
+/* Список пунктов меню */
+.menu__list {
+  padding: 18px 0 10px 0;
+}
+
+/* Элемент пункта меню */
+.menu__item {
+  padding: 5px 0;
+  margin-bottom: 16px;
+}
+
+/* Ссылка меню */
+.menu__link {
+  color: #ffffff;
+  font-weight: 400;
+  font-size: 16px;
+  line-height: 24px;
+}
+</style>
--- src/components/Track.vue
+++ src/components/Track.vue
@@ -30,3 +30,67 @@
   </div>
 </template>
 
+<style scoped>
+.main__sidebar {
+  max-width: 418px;
+  padding: 20px 90px 20px 78px;
+}
+
+.sidebar__personal {
+  display: flex;
+  flex-direction: row;
+  align-items: center;
+  justify-content: flex-end;
+  padding: 12px 0 15px 0;
+}
+
+.sidebar__personal-name {
+  font-style: normal;
+  font-weight: 400;
+  font-size: 16px;
+  line-height: 24px;
+  color: #ffffff;
+  margin-right: 16px;
+}
+
+.sidebar__icon {
+  width: 43px;
+  height: 43px;
+  background-color: #313131;
+  border-radius: 50%;
+  cursor: pointer;
+}
+
+.sidebar__block {
+  height: 100%;
+  padding: 240px 0 0 0;
+  display: flex;
+  flex-direction: column;
+  justify-content: flex-start;
+}
+
+.sidebar__list {
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+}
+
+.sidebar__item {
+  width: 250px;
+  height: 150px;
+}
+
+.sidebar__item:not(:last-child) {
+  margin-bottom: 30px;
+}
+
+.sidebar__link {
+  width: 100%;
+  height: 100%;
+}
+
+.sidebar__img {
+  width: 100%;
+  height: auto;
+}
+</style>
